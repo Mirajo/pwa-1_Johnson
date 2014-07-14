5908e21cd2d820a9bf517509f422c3e34273560d
Mirajo <mmj_chicago@yahoo.com>
2014-07-14 10:27:58 -0400

    final numbers review
    final numbers review

--- WK1/goal1_basics/js/main.js
+++ WK1/goal1_basics/js/main.js
@@ -96,35 +96,60 @@ console.log('------ strings ----------');
          console.log("Example 4: ", testString);
 
     //this example shows that all the "phase" vars above were overwritten
-
+      var num = 3;
+      var testString = 'he\'s in PWA1 ' + num;
+      console.log("Example 5: ", testString);
 
 
 // numbers (definition, concatenation +, math, numbers & strings)
 console.log('------ numbers ----------');
 
+      //var y=123e5;   //12300000
+      //var z123e-5;   //0.00123
+      
     // arithmetic operators:  +, -, /, *, % (modulo)
+       var
 
 
     //basic math
+      var counter = 10;
+      console.log("basic math: ", counter + 1);
+      console.log("basic math: ", counter);
 
 
     //quick operations with "assignment operator" +=, -=, *=, /=, %=
     //one example below, can show more examples with other assignment operators
+       var num1 = 10;
+       var num2 = 5;
+       num1 += num2;
+       console.log("+=: ", num1);
 
-
-    //increment or decrement numbers by 1 with ++ or --
+     //increment or decrement numbers by 1 with ++ or --
+       var myNum = 5;
+       console.log("myNum = ", myNum);
+       
+       myNym++;
+       console.log("myNum = ", myNum);
+       
+       myNym--;
+       console.log("myNum = ", myNum);
+       
 
     //this decrements right before it is being used
+       var num = 10;
+        --num;
+        console.log("--counter: ", num);   
+    
 
     //this decrements after it is used
-   num---;
-   console.log("counter---: ", num---);
+   //num--;
+   //console.log("counter--: ", num--);
    
 
     //comment out the above example
     //this decrements after it is being used as well
-     console.log("counter---: ", num---);
-    console.log("counter: ", num);
+       console.log("counter--: ", num--);
+       console.log("counter: ", num);
 
 
     //add a number to a string is always a string
