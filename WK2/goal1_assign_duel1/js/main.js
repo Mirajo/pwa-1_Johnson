393f6c937ae254ff1089c4bf7a9a46d66e659c17
Mirajo <mmj_chicago@yahoo.com>
2014-07-18 23:00:24 -0400

    duel players array
    duel players array

---
+++ WK2/goal1_assign_duel1/js/main.js
@@ -0,0 +1,35 @@
+/*
+     Name: Miranda Johnson
+     Date: 07 -17 -14
+     Class & Section:  PWA1-####
+     Comments: "Week 2 Duel game"
+ */
+
+
+(function (){
+
+         playerOne = {
+             'name' : "Spiderman",
+             'damage': 20,
+             'health': 100
+         } ;
+
+        playerTwo = {
+            'name': "Batman",
+            'damage': 20,
+            'health': 100
+
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+};
